69b33dd42e650bd312f28260e30f0ffe9c1f9d91
denJakob <35639237+denJakob@users.noreply.github.com>
2023-03-22 12:36:56 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+new_task_content = Element("new-task-content")
+
+def add_task_event(e):
+    if e.key == "Enter":
+        display("test")
+
+
+
+
+
+
+new_task_content.element.onkeypress = add_task_event
