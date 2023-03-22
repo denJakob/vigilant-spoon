7cba422d8a479562afd37b00385e80c4f3d9c8e3
denJakob <35639237+denJakob@users.noreply.github.com>
2023-03-22 13:03:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 new_task_content = Element("new-task-content")
 
+
+def test():
+    print(new_task_content.element.value)
+
 def add_task_event(e):
     if e.key == "Enter":
-        print(new_task_content.element.value)
+        test()
 
 
 
